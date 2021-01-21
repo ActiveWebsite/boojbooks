9b9bc5976fdfe79e3873a13fb8405318ccf3280c
Arvinder Singh <arvinder.arjinder@gmail.com>
2021-01-20 20:02:24 -0800

    Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('books', 'BookController@index')->name('books.index');
+    Route::get('books/rearrange/{bookList}', 'BookController@index')->name('books.rearrange.index');
+    Route::post('books/rearrange/{bookList}', 'BookController@rearrange')->name('books.rearrange');
+    Route::get('books/{book}', 'BookController@show')->name('books.show');
+    Route::post('books', 'BookController@store')->name('books.store');
+    Route::delete('books', 'BookController@destroy')->name('books.delete');
+});
+Route::post('/user/register', 'UserController@store')->name('user.store');
+Route::post('/user/login', 'UserController@login')->name('user.store');
+Route::post('/user/{user}/delete', '\App\Actions\Jetstream\DeleteUser@delete')->name('user.delete');
+
+
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('books', 'BookController@index')->name('books.index');
+    Route::get('books/rearrange/{bookList}', 'BookController@index')->name('books.rearrange.index');
+    Route::post('books/rearrange/{bookList}', 'BookController@rearrange')->name('books.rearrange');
+    Route::get('books/{book}', 'BookController@show')->name('books.show');
+    Route::post('books', 'BookController@store')->name('books.store');
+    Route::delete('books', 'BookController@destroy')->name('books.delete');
+});
