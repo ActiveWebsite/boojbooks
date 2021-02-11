67df425082358af35ae641f32daa858afe265640
brenjay <brenjayg@gmail.com>
2021-02-10 23:31:34 -0700

    Cleaned form

--- public/js/app.js
+++ public/js/app.js
@@ -4827,7 +4827,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
--- resources/js/Pages/Listing.vue
+++ resources/js/Pages/Listing.vue
@@ -52,7 +52,6 @@
                         <div class="fixed inset-0 transition-opacity">
                           <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
                         </div>
-                        <!-- This element is to trick the browser into centering the modal contents. -->
                         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
                         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                           <form>
